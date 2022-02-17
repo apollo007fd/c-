f4eea441a7c579e5221a20b8b07024662deb80bc
hongbozhang <hongbozhang@tencent.com>
2022-02-17 18:02:39 +0800

    add

--- leetcode/4_print_linklist_from_tail_to_head.cpp
+++ leetcode/4_print_linklist_from_tail_to_head.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <functional>
 using namespace std;
 
 // Definition for singly-linked list.
@@ -11,14 +12,29 @@ struct ListNode {
 
 class Solution {
 public:
-    void reversePrintUitl(ListNode* node, vector<int>& vints) {
-        if (node == NULL) return;
-        reversePrintUitl(node->next, vints);
-        vints.push_back(node->val);
-    }
+    // 递归法，递归函数是匿名函数
     vector<int> reversePrint(ListNode* head) {
         vector<int> ans;
-        reversePrintUitl(head, ans);
+        std::function<void(ListNode*)> glambda;
+        glambda = [&ans, &glambda] (ListNode* node) {  // 匿名函数递归需要函数本身的引用捕获
+            if (node == NULL) return;
+            glambda(node->next);
+            ans.push_back(node->val);
+        };
+        glambda(head);
         return ans;
     }
 };
+
+int main() {
+    ListNode* head = new ListNode(1);
+    auto* node = head;
+    for(int i = 2; i<5; ++i) {
+        node->next = new ListNode(i);
+        node = node->next;
+    }
+    Solution solu;
+    auto ans = solu.reversePrint(head);
+    for(auto i : ans)
+        cout << i << endl;
+}
