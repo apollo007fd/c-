9c2e6fe48f171cdc5935a9faee795e669a529212
hongbozhang <hongbozhang@tencent.com>
2022-02-23 20:52:34 +0800

    update

--- leetcode/jianzhi_offer_12_path_of_array.cpp
+++ leetcode/jianzhi_offer_12_path_of_array.cpp
@@ -5,6 +5,28 @@ using namespace std;
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
+        if (board.empty() || board[0].empty())
+            return false;
+        for (int i = 0; i < board.size(); ++i) {
+            for (int j = 0; j < board[0].size(); ++j) {
+                if (dfs(board, i, j, word, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+    bool dfs(vector<vector<char>>& board, int i, int j, string word, int k) {
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || k < 0 || board[i][j] != word[k]) return false;
+        if (k == word.size() - 1) return true;
+        board[i][j] = ' ';
+        vector<int> xi {i-1, i+1, i, i};
+        vector<int> yi {j, j, j-1, j+1};
+        bool ans = false;
+        for (int l=0; l < xi.size(); ++l) {
+            ans = ans || dfs(board, xi[l], yi[l], word, k+1);
+        }
+        board[i][j] = word[k];
+        return ans;
     }
 };
 
